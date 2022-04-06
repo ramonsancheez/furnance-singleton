06b1e499cd4775455d58e3aff720b2677eb77e0f
Ramon <rsanchezabadia@cifpfbmoll.eu>
2022-04-06 18:07:27 +0200

    feat: getter para obtener temperature

--- src/main/java/edu/poniperro/roomTemperatureFurnance/thermometer/RemoteCommandSensor.java
+++ src/main/java/edu/poniperro/roomTemperatureFurnance/thermometer/RemoteCommandSensor.java
@@ -1,6 +1,11 @@
 package edu.poniperro.roomTemperatureFurnance.thermometer;
 
 import edu.poniperro.roomTemperatureFurnance.Interfaces.Thermometer;
+import edu.poniperro.roomTemperatureFurnance.types.RoomTemperature;
 
 public class RemoteCommandSensor implements Thermometer {
+    @Override
+    public double getTemperature(RoomTemperature temperature) {
+        return temperature.getTemperature();
+    }
 }
