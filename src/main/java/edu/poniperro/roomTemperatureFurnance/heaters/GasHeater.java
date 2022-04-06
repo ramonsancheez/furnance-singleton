2b6a3eb31d7430443ddc71d0aa9e8e9461b5b1f9
Ramon <rsanchezabadia@cifpfbmoll.eu>
2022-04-06 18:04:51 +0200

    feat: metodos para enfriar y calentar

--- src/main/java/edu/poniperro/roomTemperatureFurnance/heaters/GasHeater.java
+++ src/main/java/edu/poniperro/roomTemperatureFurnance/heaters/GasHeater.java
@@ -1,6 +1,16 @@
 package edu.poniperro.roomTemperatureFurnance.heaters;
 
 import edu.poniperro.roomTemperatureFurnance.Interfaces.Heater;
+import edu.poniperro.roomTemperatureFurnance.types.RoomTemperature;
 
 public class GasHeater implements Heater {
+    @Override
+    public void heatUp(RoomTemperature temperature) {
+        temperature.setTemperature(temperature.getTemperature() + 1);
+    }
+
+    @Override
+    public void coolDown(RoomTemperature temperature) {
+        temperature.setTemperature(temperature.getTemperature() - 1);
+    }
 }
--- src/main/java/edu/poniperro/roomTemperatureFurnance/heaters/Jedi.java
+++ src/main/java/edu/poniperro/roomTemperatureFurnance/heaters/Jedi.java
@@ -1,8 +1,19 @@
 package edu.poniperro.roomTemperatureFurnance.heaters;
 
 import edu.poniperro.roomTemperatureFurnance.Interfaces.Heater;
+import edu.poniperro.roomTemperatureFurnance.types.RoomTemperature;
 
 public class Jedi implements Heater {
 
-
+    @Override
+    public void heatUp(RoomTemperature temperature) {
+        temperature.setTemperature(temperature.getTemperature() + 1);
+    }
+    @Override
+    public void coolDown(RoomTemperature temperature) {
+        temperature.setTemperature(temperature.getTemperature() - 1);
+    }
+    public void speak() {
+        System.out.println("Hola, soy un Jedi");
+    }
 }
